46eb548886e9152d375f8c719570b621d70875ab
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-06-25 06:40:03 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -1,8 +1,8 @@
-# 22-6-2018 JHZ
+# 25-6-2018 JHZ
 
 git add README.md
 git commit -m "README"
-for d in C C++ java python st.sh
+for d in doc C C++ java python st.sh
 do
    git add $d
    git commit -m "$d"
