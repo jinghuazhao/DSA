ccf76c1e5985c332bf02ba50283c9980ea65dbbf
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2025-04-12 20:47:23 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -1,4 +1,21 @@
-# 25-6-2018 JHZ
+# 12-4-2025 JHZ
+
+function github_pages()
+{
+  module load python/3.8
+  source ~/rds/public_databases/software/py38/bin/activate
+# pip install mkdocs-mermaid2-plugin
+}
+
+if [ "$(uname -n | sed 's/-[0-9]*$//')" == "login-q" ]; then
+   module load ceuadmin/libssh/0.10.6-icelake
+   module load ceuadmin/openssh/9.7p1-icelake
+   module load ceuadmin/krb5
+fi
+
+github_pages
+mkdocs build
+mkdocs gh-deploy
 
 git add README.md
 git commit -m "README"
